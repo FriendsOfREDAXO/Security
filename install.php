429b5de2e4260b975aa576755cd855ea0f96bae0
Thomas Skerbis <thomas.skerbis@mac.com>
2024-01-26 10:25:33 +0100

    Write securit to setup addOns system config

--- install.php
+++ install.php
@@ -1,4 +1,17 @@
 <?php
+$addon = rex_addon::get('maintenance');
+
+// Write securit to setup addOns system config
+$config_file = rex_path::coreData('config.yml');
+$config = rex_file::get($config_file);
+if ($config !== null) {
+    $data = rex_string::yamlDecode($config);
+    if (in_array("securit", $data['setup_addons'], true)) {
+    } else {
+        $data['setup_addons'][] = 'securit';
+        rex_file::put($config_file, rex_string::yamlEncode($data, 3));
+    }
+}
 
 rex_yform_manager_table::deleteCache();
 
