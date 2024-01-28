a23ac53f4bc0d20f499679e40fbf7d3de08ff774
Jan Kristinus <jan.kristinus@yakamara.de>
2024-01-28 14:18:08 +0100

    CS

--- pages/overview.php
+++ pages/overview.php
@@ -6,35 +6,6 @@ namespace FriendsOfRedaxo\Securit;
  * @var \rex_addon $this
  */
 
-// Frontendsessions killen können
-// Allen Usern neue SessionID geben
-
-// SESSION HANDLING um alte Sessions zu löschen
-// -> FE / BE / beides
-// -> Zeitraum in Session setzen wenn nicht gesetzt
-// -> wenn Zeitpunkt erreicht -> Session killen
-
-// Benachrichtigung bei bestimmten EPs aktivieren
-// z.B. EP erreicht, dann User loggen und Email raus
-
-// 2 Factor aktivierbar fürs BE machen
-// Welche Authentifizierung
-// Email mit Code, Authenticator
-
-// Passwort vergessen
-// E-Mail Info durchschicken.
-
-// Failed Logins
-// Benachrichtigung
-// log
-// IP Block bei bestimmten Mengen in bestimmter Zeit
-
-// Passwortregeln erweitern
-// - Reenter Password after x Months
-// - alte hashes speichern und prüfen ob das passwort schon benutzt wurde
-
-// Header ergänzen
-
 $content = [];
 $content[] = '<h3>'.$this->i18n('frontend_access'). '</h3>';
 $content[] = 1 == \rex_config::get('securit', 'fe_access_status') ? '<div class="alert alert-success">'.$this->i18n('enabled').'</div>' : '<div class="alert alert-info" style=";">'.$this->i18n('disabled').'</div>';
@@ -49,7 +20,7 @@ $content[] = '<h3>'.$this->i18n('ip_access_frontend'). '</h3>';
 $content[] = IPAccess::isActive() ? '<div class="alert alert-success">'.$this->i18n('enabled').'</div>' : '<div class="alert alert-info" style=";">'.$this->i18n('disabled').'</div>';
 
 $content[] = '<h3>'.$this->i18n('ip_access_backend'). '</h3>';
-$content[] = IPAccess::isActive("backend") ? '<div class="alert alert-success">'.$this->i18n('enabled').'</div>' : '<div class="alert alert-warning" style=";">'.$this->i18n('disabled').'</div>';
+$content[] = IPAccess::isActive('backend') ? '<div class="alert alert-success">'.$this->i18n('enabled').'</div>' : '<div class="alert alert-warning" style=";">'.$this->i18n('disabled').'</div>';
 
 $content[] = '<h3>'.$this->i18n('health_link'). '</h3>';
 $content[] = '<div class="alert alert-info"><a href="'.Health::getLink().'">'.Health::getLink().'</a></div>';
