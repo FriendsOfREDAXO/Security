6f8b5b600f8ad8e9289014f462a7c2d0f01d0484
Jan Kristinus <jan.kristinus@yakamara.de>
2024-03-15 15:29:40 +0100

    tableset import dateiname war noch ysecure

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 
 rex_yform_manager_table::deleteCache();
 
-$content = rex_file::get(rex_path::addon('security', 'install/yform/yform_manager_tableset_export_tables_rex_ysecure_ip_access.json'));
+$content = rex_file::get(rex_path::addon('security', 'install/yform/yform_manager_tableset_export_tables_rex_security_ip_access.json'));
 // @phpstan-ignore-next-line
 if ($content && '' != $content) {
     rex_yform_manager_table_api::importTablesets($content);
